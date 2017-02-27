8bf3be4fa46e4093ba21d1831cabfbebbfa218cf
Rana <vranacse@gmail.com>
2017-02-27 16:33:05 +0530

    Initial Commit

---
+++ app/src/main/obj/local/armeabi-v7a/objs/ffmpeg/ffmpeg-jni.o.d
@@ -0,0 +1,110 @@
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/obj/local/armeabi-v7a/objs/ffmpeg/ffmpeg-jni.o: \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/ffmpeg-jni.c \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavcodec/avcodec.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavutil/samplefmt.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/avutil.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/common.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/attributes.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/version.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/macros.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavutil/avconfig.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\config.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/intmath.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/arm/intmath.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/mem.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/error.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/internal.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/timer.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/log.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/arm/timer.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/cpu.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/dict.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/libm.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/intfloat.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/rational.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/mathematics.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/pixfmt.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/old_pix_fmts.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavutil/buffer.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavutil/channel_layout.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavutil/frame.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavcodec/version.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavcodec/old_codec_ids.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavformat/avformat.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil\time.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavformat/avio.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavutil/common.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavformat/version.h \
+  C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/opt.h
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavcodec/avcodec.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavutil/samplefmt.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/avutil.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/common.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/attributes.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/version.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/macros.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavutil/avconfig.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\config.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/intmath.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/arm/intmath.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/mem.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/error.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/internal.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/timer.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/log.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/arm/timer.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/cpu.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/dict.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/libm.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/intfloat.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/rational.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/mathematics.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/pixfmt.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/old_pix_fmts.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavutil/buffer.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavutil/channel_layout.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavutil/frame.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavcodec/version.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavcodec/old_codec_ids.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavformat/avformat.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil\time.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavformat/avio.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavutil/common.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni\libavformat/version.h:
+
+C:/Users/Rana/Downloads/Android-Youtube-LiveStream-master/MainActivity/app/src/main/jni/libavutil/opt.h:
